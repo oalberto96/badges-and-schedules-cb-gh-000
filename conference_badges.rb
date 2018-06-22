a90c6ec93fb72987b478f2b1b706d360f0993e0f
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 02:35:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,6 @@ def assign_rooms(speakers)
 end
 
 def printer(speakers)
-  batch_badge_creator(speakers)
-  assign_rooms(speakers)
+  puts batch_badge_creator(speakers)
+  puts assign_rooms(speakers)
 end
