5e501d0c6e4b7a3321921190117888eb2d3f38d6
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 02:39:50 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each{|name| badges << badge_maker(name) }
+  badges
+end
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index do |speaker, index|
+    rooms << "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+  end
+  rooms
+end
+
+def printer(speakers)
+  badges = batch_badge_creator(speakers)
+  rooms = assign_rooms(speakers)
+  badges.each{|badge| puts badge}
+  rooms.each{|room| puts room}
+end
