9d7c694b0809ab568216cf441be58c3acdc5e673
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-22 02:32:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,12 +6,12 @@ end
 def batch_badge_creator(names)
   badges = []
   names.each{|name| badges << badge_maker(name) }
+  badges
 end
 
 def assign_rooms(speakers)
   rooms = []
   speakers.each_with_index do |speaker, index|
     rooms < "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
-    room += 1
   end
 end
